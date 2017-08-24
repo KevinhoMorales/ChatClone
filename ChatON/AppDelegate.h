e4e52a80a9302649c6c1dcc81f78fb589fc0e341
Rakia Naeem <rakia@appimize.com>
2017-08-24 17:35:04 +0500

    Initial Commit

---
+++ ChatON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatON
+//
+//  Created by Saba Amber on 24/08/2017.
+//  Copyright © 2017 SabaAmber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatON/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatON
+//
+//  Created by Saba Amber on 24/08/2017.
+//  Copyright © 2017 SabaAmber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
